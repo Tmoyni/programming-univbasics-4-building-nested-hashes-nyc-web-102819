98538fab5a7f867c7b165e7ab0ebda352ee55c5d
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-04 21:09:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,9 +39,9 @@ def monopoly_with_fourth_tier
 	     :reading_railroad=>{
 	       'mortgage_value'=>'$100'
 	     },
-	     :pennsylvania_railroad=>[
+	     :pennsylvania_railroad=>{
 	       'mortgage_value'=>'$200'
-	     ],
+	     },
 	     :shortline_railroad=>{
 	       'mortgage_value'=>'$200'
 	     },
