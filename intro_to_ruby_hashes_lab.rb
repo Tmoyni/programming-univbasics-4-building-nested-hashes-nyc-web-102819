60e06c6f71d84456a9bca73c09dfb1efeffb015a
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-04 19:55:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  base_hash[monopoly {:railroads => {:new_key}] = "New Value"
+  base_hash = monopoly = {:railroads => {} }
 end
 
 def monopoly_with_third_tier
